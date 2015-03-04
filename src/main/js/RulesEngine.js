f7ec98856e47d3abbdd258dd6c23ab0777279004
Nick Grealy <nickgrealy@gmail.com>
2015-03-04 00:36:44 +0000

    Extracted polyfill functions.

--- src/main/js/RulesEngine.js
+++ src/main/js/RulesEngine.js
@@ -5,77 +5,50 @@ Standalone implementation of a rules engine.
 Author: Nick Grealy
 Date: 04/07/2014
 
-Example Usage:
-
-var then1 = function(){console.log('exec scenario1'); return 1; },
-    then2 = function(){console.log('exec scenario2'); return 2; },
-    then3 = function(){console.log('exec scenario3'); return 3; };
-
-var rules = [
-  ['outcome',       'documentToProduce'],
-  ['Review Held',   'ROA Short Form',       then1],
-  ['Review Held',   'ROA Short Formx',      then2],
-  ['Review Held',   ,                       then3]
-]
-
-var engine = RulesEngine.build(rules)
-var result = engine.evaluate({outcome: 'Review Held', documentToProduce: 'ROA Short Formxx'})  // exec scenario1
-// result = 1
+TODO:
+- use foreach method
 
  */
-if (!Array.prototype.indexOf) {
-    Array.prototype.indexOf = function(obj, start) {
-         for (var i = (start || 0), j = this.length; i < j; i++) {
-             if (this[i] === obj) { return i; }
-         }
-         return -1;
-    }
-}
-if (!Array.prototype.compare){
-    Array.prototype.compare = function(array2) {
-        var inter = []
-        var tmp1 = this.slice(0)
-        var tmp2 = array2.slice(0)
-        for (var i = tmp1.length-1; i > -1; i--){
-            var idx = tmp2.indexOf(this[i])
-            // destructive, only count matches once
-            if (idx > -1){
-                tmp1.splice(i, 1)
-                tmp2.splice(idx, 1)
-                inter[inter.length] = this[i]
-            }
-        }
-        var areEqual = tmp1.length == 0 && tmp2.length == 0
-        return {intersection: inter, leftComplement: tmp1, rightComplement: tmp2, left: tmp1, right: tmp2, equal: areEqual}
-    }
-}
  
+var isUndef = function(object){ return typeof object === 'undefined'; };
+var isNotUndef = function(object){ return typeof object !== 'undefined'; };
+var isFn = function(object){ return typeof object === 'function'; };
+
 var RulesEngine = {
 
-    wildcard:   function(){return true},
-    all:        function(){return true},
-    debug:      false,
+	version  : '1.0.2',
+	wildcard : function(){return true},
+	all      : function(){return true},
+	debug    : false,
 
+    buildEngine: function(inputsMap){
+		return this.build(inputsMap.rules, inputsMap.defaultFn, inputsMap.multiMode)
+	},
+	
     build: function(twoDimRulesArray, defaultFunction, multiMode){
-        var thiz = this, logDebug = typeof console !== 'undefined' && thiz.debug
-        if (logDebug){ console.log('=== Building Rules Engine ===') }
-        multiMode = typeof multiMode === 'undefined' ? false : multiMode
+        var thiz = this, logDebug = isNotUndef(console) && thiz.debug
+        if (logDebug){ console.log('-> === Building Rules Engine ===') }
+        multiMode = isUndef(multiMode) ? false : multiMode
         var rules = this.buildRules(twoDimRulesArray, defaultFunction)
         return {
+			version: thiz.version,
+			privRules: rules,
             evaluate: function(inputs){
-                var logDebug = typeof console !== 'undefined' && thiz.debug
-                if (typeof inputs === 'undefined' || inputs == null){
+                var logDebug = isNotUndef(console) && thiz.debug
+                if (isUndef(inputs) || inputs == null){
                     if (logDebug){ console.error('Cannot evaluate null or undefined input!') }
                     return
                 }
                 if (logDebug){ console.log('--- Evaluating inputs= '+thiz.objectToString(inputs)+' ---') }
                 var returnResponse = null
+				var ruleMatched = false
                 for (var i = 0; i < rules.length; i++){
                     if (thiz.equals(rules[i].condition, inputs)){    // if we have a match
-                        if (typeof rules[i].then !== 'undefined'){
-                            if (console){ console.log('Matched rule #' + i + ' - inputs=' + thiz.objectToString(inputs)) }
-                            var response = rules[i].then(rules[i].params)
-                            if (logDebug){ console.log('Found rule match: ' + rules[i].toString() + ' output:' + response) }
+						ruleMatched = true
+                        if (isNotUndef(rules[i].then)){
+                            if (isNotUndef(console)){ console.log('-> Matched rule #' + i + ' - inputs=' + thiz.objectToString(inputs)) }
+                            var response = rules[i].then(rules[i].params, inputs)
+                            if (logDebug){ console.log('-> Found rule match: ' + rules[i].toString() + ' output:' + response) }
                             returnResponse = response
                         } else {
                             if (logDebug){ console.log('Cannot execute undefined function!') }
@@ -85,8 +58,8 @@ var RulesEngine = {
                         }
                     }
                 }
-                if (returnResponse == null){
-                    if (console){ console.log('No rule matched - inputs=' + thiz.objectToString(inputs)) }
+                if (!ruleMatched){
+                    if (isNotUndef(console)){ console.log('-> No rule matched - inputs=' + thiz.objectToString(inputs)) }
                 }
                 return returnResponse
             }
@@ -94,33 +67,33 @@ var RulesEngine = {
     },
 
     buildRules: function(twoDimensionalArray, defaultFunction){
-        var thiz = this, logDebug = typeof console !== 'undefined' && thiz.debug
+        var thiz = this, logDebug = isNotUndef(console) && thiz.debug
         var rules = []
-        if (typeof twoDimensionalArray === 'undefined' || twoDimensionalArray == null || typeof twoDimensionalArray.length === 'undefined' || twoDimensionalArray.length == 0){
+        if (isUndef(twoDimensionalArray) || twoDimensionalArray == null || isUndef(twoDimensionalArray.length) || twoDimensionalArray.length == 0){
             throw 'twoDimensionalArray must be a two dimensional array!'
         }
         var tmp = twoDimensionalArray[0] // first row is the list of fields
-        var nonStrings = thiz.findAll(tmp, function(a){ return typeof a !== 'string' })
+        var nonStrings = tmp.filter(function(a){ return typeof a !== 'string' })
         if (nonStrings.length > 0){
             var mesg = 'Header row must only contain strings! headers=' + tmp.join(',')
             if (logDebug){ console.error(mesg) }
             throw mesg
         }
-        var fieldNames = thiz.findAll(tmp, function(a){ return a.indexOf && a.indexOf('p_') === -1 })
-        var inputNames = thiz.collect(tmp, function(a){ if (a.indexOf && a.indexOf('p_') === 0){ return a.substring(2) } }) // this performance could be improved
+        var fieldNames = tmp.filter(function(a){ return a.indexOf && a.indexOf('p_') === -1 })
+        var inputNames = tmp.map(function(a){ if (a.indexOf && a.indexOf('p_') === 0){ return a.substring(2) } }).filter(isNotUndef) // this performance could be improved
 		if (fieldNames.length == 0){
 			throw 'Could not find any field names! headerRow=' + tmp.join(',')
 		}
         var expectedLength = fieldNames.length + inputNames.length
         for (var i = 1; i < twoDimensionalArray.length; i++){
-            if (typeof twoDimensionalArray[i] !== 'undefined'){
+            if (isNotUndef(twoDimensionalArray[i])){
                 var actualLen = twoDimensionalArray[i].length
                 if (actualLen != expectedLength && actualLen != expectedLength+1){
                     throw 'Expected length='+expectedLength+' but found length='+twoDimensionalArray[i].length+'! row#='+i
                 }
                 var tmp = this.buildRule(fieldNames, inputNames, twoDimensionalArray[i], defaultFunction)
                 if (logDebug){ console.log(tmp.toString()) }
-                if (typeof tmp !== 'undefined' && tmp !== null){
+                if (isNotUndef(tmp) && tmp !== null){
                     rules[rules.length] = tmp
                 }
             } else {
@@ -131,12 +104,12 @@ var RulesEngine = {
     },
 
     buildRule: function(fieldNames, inputNames, conditions, defaultCondition){
-        var thiz = this, logDebug = typeof console !== 'undefined' && thiz.debug
-        if (typeof defaultCondition === 'undefined'){
-            if (logDebug){ console.log('Found undefined defaultFunction, replacing with placeholder...') }
+        var thiz = this, logDebug = isNotUndef(console) && thiz.debug
+        if (isUndef(defaultCondition)){
+            if (logDebug){ console.log('-> Found undefined defaultFunction, replacing with placeholder...') }
             defaultCondition = function(){}
         }
-        if (typeof conditions !== 'undefined'){
+        if (isNotUndef(conditions)){
             var fnlen = fieldNames.length, inlen = inputNames.length
             var conditionObject = {}
             for (var i = 0; i < fnlen; i++){
@@ -147,95 +120,59 @@ var RulesEngine = {
                 inputObject[inputNames[i]] = conditions[i + fnlen]
             }
             var thenCondition = defaultCondition
-            if ((fnlen + inlen) < conditions.length && typeof conditions[conditions.length-1] === 'function'){
+            if ((fnlen + inlen) < conditions.length && isFn(conditions[conditions.length-1])){
                 thenCondition = conditions[conditions.length-1]
-            }
+				if (logDebug){ console.log("-> Using override function.") }
+            } else {
+				if (logDebug){ console.log("-> Using default function - check1=" + thiz.objectToString(conditions)) }
+			}
             return {condition: conditionObject, params: inputObject, then: thenCondition, toString: function(){
                 return "Rule={ conditions: " + thiz.objectToString(conditionObject) + ", params: " + thiz.objectToString(inputObject) + ", then: "+ typeof thenCondition+" }"
             }}
         }
     },
     
+    /* -- N.B. These below methods should be in a common library. -- */
+    
     objectToString: function(map){
-        var thiz = this, logDebug = typeof console !== 'undefined' && thiz.debug
-        var keys = this.getKeys(map)
+        var thiz = this, logDebug = isNotUndef(console) && thiz.debug
+        var keys = Object.keys(map)
         var string = '('
         if (keys.length > 0){
-            string += keys[0]+'='+ (typeof map[keys[0]] === 'function' ? 'function' : map[keys[0]])
+            string += keys[0]+'='+ (isFn(map[keys[0]]) ? 'function' : map[keys[0]])
         }
         if (keys.length > 1){
             for (var i = 1; i < keys.length; i++){
-                string += ','+keys[i]+'='+(typeof map[keys[i]] === 'function' ? 'function' : map[keys[i]])
+                string += ','+keys[i]+'='+(isFn(map[keys[i]]) ? 'function' : map[keys[i]])
             }
         }
         string += ')'
         return string
     },
-    
-    getKeys: function(obj){
-        var thiz = this, logDebug = typeof console !== 'undefined' && thiz.debug
-        var keys = []
-        for(var key in obj){
-            if (obj.hasOwnProperty(key)) {
-                keys[keys.length] = key
-            }
-        }
-        return keys
-    },
-    
-    /* -- N.B. These below methods should be in a common library. -- */
 
     /* does a deep match, of 1st level fields, of an object */
     equals: function(a, b){
-        var thiz = this, logDebug = typeof console !== 'undefined' && thiz.debug
-        var keys = thiz.getKeys(a)
-        if (logDebug){ console.log('Checking keys: ' + keys.join(',')) }
+        var thiz = this, logDebug = isNotUndef(console) && thiz.debug
+        var keys = Object.keys(a)
+        if (logDebug){ console.log('-> Checking keys: ' + keys.join(',')) }
         for (var i = 0; i < keys.length; i++){
             var field = keys[i]
             // check if a is null or undefined, if so, compare using ===
-            var aIsNull = typeof a[field] === 'undefined' || a[field] === null
+            var aIsNull = isUndef(a[field]) || a[field] === null
             // check if the objects match
             var matches = (aIsNull ? a[field] === b[field] : a[field] == b[field]) 
-                || (typeof a[field] === 'function' && a[field](b[field]))   // function
+                || (isFn(a[field]) && a[field](b[field]))   // function
                 || (a[field] instanceof Array && b[field] instanceof Array && a[field].compare(b[field]).equal)   // array
             if (!matches){
-                if (logDebug){ console.log('Rejecting match on field [rule, actual] -> ' + field + ' [' + a[field] + ' != ' + b[field] + ']') }
+                if (logDebug){ console.log('-> Rejecting match on field [rule, actual] -> ' + field + ' [' + a[field] + ' != ' + b[field] + ']') }
                 return false;
             } else {
-                if (logDebug){ console.log('Found match on field [rule, actual] -> ' + field + ' [' + a[field] + ' == ' + b[field] + ']') }
+                if (logDebug){ console.log('-> Found match on field [rule, actual] -> ' + field + ' [' + a[field] + ' == ' + b[field] + ']') }
             }
         }
         // everything matches, return true!
         return true;
-    },
-    
-    /* finds all matches - see groovy! */ 
-    findAll: function(list, closure){
-        var thiz = this, logDebug = typeof console !== 'undefined' && thiz.debug
-        var tmp=[], clos = closure;
-        if (typeof list === 'undefined' || list == null){
-            return tmp
-        }
-        for (var i=0;i<list.length;i++){
-            if (clos(list[i])){
-                tmp[tmp.length] = list[i];
-            }
-        }
-        return tmp;
-    },
-    
-    /* collects all non-undefined objects */ 
-    collect: function(list, closure){
-        var thiz = this, logDebug = typeof console !== 'undefined' && thiz.debug
-        var tmp=[], clos = closure;
-        if (typeof list === 'undefined' || list == null){
-            return tmp
-        }
-        for (var i=0;i<list.length;i++){
-            var val = clos(list[i], i)
-            if (typeof val !== 'undefined'){ tmp[tmp.length] = val };
-        }
-        return tmp;
     }
+    
 };
 var _re = RulesEngine
