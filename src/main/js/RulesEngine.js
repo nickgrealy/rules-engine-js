3b38b925b3082d687da61ce6968827d8668b8b6c
Nick Grealy <nickgrealy@gmail.com>
2015-03-04 03:53:44 +0000

    updated rules

--- src/main/js/RulesEngine.js
+++ src/main/js/RulesEngine.js
@@ -5,14 +5,10 @@ Standalone implementation of a rules engine.
 Author: Nick Grealy
 Date: 04/07/2014
 
-TODO:
-- use foreach method
-
  */
- 
-var isUndef = function(object){ return typeof object === 'undefined'; };
+var isUndef    = function(object){ return typeof object === 'undefined'; };
 var isNotUndef = function(object){ return typeof object !== 'undefined'; };
-var isFn = function(object){ return typeof object === 'function'; };
+var isFn       = function(object){ return typeof object === 'function';  };
 
 var RulesEngine = {
 
@@ -36,19 +32,20 @@ var RulesEngine = {
             evaluate: function(inputs){
                 var logDebug = isNotUndef(console) && thiz.debug
                 if (isUndef(inputs) || inputs == null){
-                    if (logDebug){ console.error('Cannot evaluate null or undefined input!') }
+                    if (isNotUndef(console)){ console.error('-> Cannot evaluate null or undefined input!') }
                     return
                 }
-                if (logDebug){ console.log('--- Evaluating inputs= '+thiz.objectToString(inputs)+' ---') }
+                if (logDebug){ console.log('-> Evaluating inputs= '+thiz.objectToString(inputs)+' ---') }
                 var returnResponse = null
 				var ruleMatched = false
                 for (var i = 0; i < rules.length; i++){
-                    if (thiz.equals(rules[i].condition, inputs)){    // if we have a match
+                    var rule = rules[i]
+                    if (thiz.equals(rule.condition, inputs)){    // if we have a match
 						ruleMatched = true
-                        if (isNotUndef(rules[i].then)){
+                        if (isNotUndef(rule.then)){
                             if (isNotUndef(console)){ console.log('-> Matched rule #' + i + ' - inputs=' + thiz.objectToString(inputs)) }
-                            var response = rules[i].then(rules[i].params, inputs)
-                            if (logDebug){ console.log('-> Found rule match: ' + rules[i].toString() + ' output:' + response) }
+                            var response = rule.then(rule.params, inputs)
+                            if (logDebug){ console.log('-> Found rule match: ' + rule.toString() + ' output:' + response) }
                             returnResponse = response
                         } else {
                             if (logDebug){ console.log('Cannot execute undefined function!') }
@@ -72,29 +69,30 @@ var RulesEngine = {
         if (isUndef(twoDimensionalArray) || twoDimensionalArray == null || isUndef(twoDimensionalArray.length) || twoDimensionalArray.length == 0){
             throw 'twoDimensionalArray must be a two dimensional array!'
         }
-        var tmp = twoDimensionalArray[0] // first row is the list of fields
-        var nonStrings = tmp.filter(function(a){ return typeof a !== 'string' })
+        var headers = twoDimensionalArray[0] // first row is the list of fields
+        var nonStrings = headers.filter(function(a){ return typeof a !== 'string' })
         if (nonStrings.length > 0){
-            var mesg = 'Header row must only contain strings! headers=' + tmp.join(',')
+            var mesg = 'Header row must only contain strings! headers=' + headers.join(',')
             if (logDebug){ console.error(mesg) }
             throw mesg
         }
-        var fieldNames = tmp.filter(function(a){ return a.indexOf && a.indexOf('p_') === -1 })
-        var inputNames = tmp.map(function(a){ if (a.indexOf && a.indexOf('p_') === 0){ return a.substring(2) } }).filter(isNotUndef) // this performance could be improved
+        var fieldNames = headers.filter(function(a){ return a.indexOf && a.indexOf('p_') === -1 })
+        var inputNames = headers.map(function(a){ if (a.indexOf && a.indexOf('p_') === 0){ return a.substring(2) } }).filter(isNotUndef) // this performance could be improved
 		if (fieldNames.length == 0){
-			throw 'Could not find any field names! headerRow=' + tmp.join(',')
+			throw 'Could not find any field names! headerRow=' + headers.join(',')
 		}
         var expectedLength = fieldNames.length + inputNames.length
         for (var i = 1; i < twoDimensionalArray.length; i++){
-            if (isNotUndef(twoDimensionalArray[i])){
-                var actualLen = twoDimensionalArray[i].length
+            var row = twoDimensionalArray[i]
+            if (isNotUndef(row)){
+                var actualLen = row.length
                 if (actualLen != expectedLength && actualLen != expectedLength+1){
-                    throw 'Expected length='+expectedLength+' but found length='+twoDimensionalArray[i].length+'! row#='+i
+                    throw 'Expected length='+expectedLength+' but found length='+row.length+'! row#='+i
                 }
-                var tmp = this.buildRule(fieldNames, inputNames, twoDimensionalArray[i], defaultFunction)
-                if (logDebug){ console.log(tmp.toString()) }
-                if (isNotUndef(tmp) && tmp !== null){
-                    rules[rules.length] = tmp
+                var rule = this.buildRule(fieldNames, inputNames, row, defaultFunction)
+                if (logDebug){ console.log(rule.toString()) }
+                if (isNotUndef(rule) && rule !== null){
+                    rules[rules.length] = rule
                 }
             } else {
                 if (logDebug){ console.log('Rejecting undefined row: #' + i + ' headers=' + fieldNames.join(',')) }
@@ -110,20 +108,20 @@ var RulesEngine = {
             defaultCondition = function(){}
         }
         if (isNotUndef(conditions)){
-            var fnlen = fieldNames.length, inlen = inputNames.length
-            var conditionObject = {}
-            for (var i = 0; i < fnlen; i++){
-                conditionObject[fieldNames[i]] = conditions[i]
-            }
-            var inputObject = {}
-            for (var i = 0; i < inlen; i++){
-                inputObject[inputNames[i]] = conditions[i + fnlen]
-            }
-            var thenCondition = defaultCondition
-            if ((fnlen + inlen) < conditions.length && isFn(conditions[conditions.length-1])){
-                thenCondition = conditions[conditions.length-1]
+            // build condition and input objects
+            var fnlen = fieldNames.length
+            var inlen = inputNames.length
+            var conditionObject = {}, inputObject = {}
+            fieldNames.forEach(function(fld, i){ conditionObject[fld] = conditions[i] })
+            inputNames.forEach(function(fld, i){ inputObject[fld] = conditions[i+fnlen] })
+            // determine then function
+            var thenCondition
+            var overrideCondition = conditions[conditions.length-1]
+            if ((fnlen + inlen) < conditions.length && isFn(overrideCondition)){
+                thenCondition = overrideCondition
 				if (logDebug){ console.log("-> Using override function.") }
             } else {
+                thenCondition = defaultCondition
 				if (logDebug){ console.log("-> Using default function - check1=" + thiz.objectToString(conditions)) }
 			}
             return {condition: conditionObject, params: inputObject, then: thenCondition, toString: function(){
@@ -134,20 +132,10 @@ var RulesEngine = {
     
     /* -- N.B. These below methods should be in a common library. -- */
     
-    objectToString: function(map){
-        var thiz = this, logDebug = isNotUndef(console) && thiz.debug
-        var keys = Object.keys(map)
-        var string = '('
-        if (keys.length > 0){
-            string += keys[0]+'='+ (isFn(map[keys[0]]) ? 'function' : map[keys[0]])
-        }
-        if (keys.length > 1){
-            for (var i = 1; i < keys.length; i++){
-                string += ','+keys[i]+'='+(isFn(map[keys[i]]) ? 'function' : map[keys[i]])
-            }
-        }
-        string += ')'
-        return string
+    objectToString: function(object){
+        return "("+Object.keys(object).map(
+            function(key){ return key+"="+(isFn(object[key]) ? 'function' : object[key]); }
+            ).join(",")+")"
     },
 
     /* does a deep match, of 1st level fields, of an object */
